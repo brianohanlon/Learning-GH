7db7f0cc0afd62dccaa3ab45b27f1478468249c6
Brian <brianohanlon22@gmail.com>
2022-08-22 12:50:35 -0400

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Brian OHanlon on 8/22/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
